bd49202c50215012977dcfcf527641c4105b59a5
Ben <bdpowell@email.wm.edu>
2016-04-06 01:32:12 +0000

    first skeleton of code

--- turret.py
+++ turret.py
@@ -1,13 +1,37 @@
 #http://computers.tutsplus.com/tutorials/controlling-dc-motors-using-python-with-a-raspberry-pi--cms-20051
-
 import RPi.GPIO as GPIO
 from time import sleep
+
+def move_up(degrees):
+	pass
+
+def move_down(degrees):
+	pass
+
+def move_right(degrees):
+	pass
+
+def move_left(degrees):
+	pass
+
+def shoot_short():
+	pass
+
+def shoot_long(time):
+	pass
  
 GPIO.setmode(GPIO.BOARD)
  
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
+GPIO_port1 = 16
+GPIO_port2 = 18
+GPIO_port3 = 22
+
+# 
+Motor1A = GPIO_port1
+# 
+Motor1B = GPIO_port2
+#enable pin - no motors on unless this is on
+Motor1E = GPIO_port3
  
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
